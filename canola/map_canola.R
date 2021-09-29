76eeb3d0d39bf20220a2c7d8ee90d3f20158c06b
Yedomon Ange Bovys Zoclanclounon <45568831+Yedomon@users.noreply.github.com>
2021-09-30 08:21:22 +0900

    Add files via upload

--- canola/map_canola.R
+++ canola/map_canola.R
@@ -42,6 +42,28 @@ prod = ggplot(data = worldmap) +
   theme_minimal() 
 prod
 
+
+
+library(RColorBrewer)
+# Définissez le nombre de couleurs que vous voulez
+nb.cols <- 22
+mycolors <- colorRampPalette(brewer.pal(8, "Set2"))(nb.cols)
+
+prod1 = ggplot(data = worldmap) +
+  geom_sf(fill="#F0F3F4", colour="white", size=0.5) +
+  geom_point(data = data_prod, aes(x=long, y=lat, size= Production, color = Country)) +
+  scale_size_continuous(name = expression(Production~(10^5~tonnes))) + #   , breaks = mybreaks) +
+  #geom_text_repel(data=data_prod, aes(x=long, y=lat, label=country))+
+  scale_color_manual(values = mycolors) +
+  labs(y = "", x = "") +
+  ggtitle('(A)') +
+  theme_minimal() #+
+  #theme(legend.position="bottom")
+prod1
+
+
+
+
 area = ggplot(data = worldmap) +
   geom_sf(fill="#F0F3F4", colour="white", size=0.5) +
   geom_point(data = data_area, aes(x=long, y=lat, size= Area), color='#28B463') +
@@ -51,14 +73,39 @@ area = ggplot(data = worldmap) +
   ggtitle('(B)') +
   theme_minimal() 
 
-area
+
+
+area2 = ggplot(data = worldmap) +
+  geom_sf(fill="#F0F3F4", colour="white", size=0.5) +
+  geom_point(data = data_area, aes(x=long, y=lat, size= Area, color=Country)) +
+  scale_size_continuous(name = expression(Cultivated~area~(10^5~hectares)))+
+  #geom_text_repel(data=data_prod, aes(x=long, y=lat, label=country))+
+  labs(y = "", x = "") +
+  ggtitle('(B)') +
+  theme_minimal() +
+  scale_color_manual(values = mycolors)# +
+  #theme(legend.position="bottom")
+
+
+
+area2
 
 
 figure = (prod) /
   (area)
 
+figure1 = (prod1) /
+  (area2)
+
+figure1
+
 ggsave(figure, file = "Map_canola.pdf", limitsize = FALSE, width = 12, height = 10.5, dpi=1500 )
 ggsave(figure, file = "Map_canola.png", limitsize = FALSE, width = 12, height = 10.5, type = "cairo-png", dpi=1500)
+
+
+ggsave(figure1, file = "Map_canola1.pdf", limitsize = FALSE, width = 12, height = 12, dpi=1500 )
+ggsave(figure1, file = "Map_canola1.png", limitsize = FALSE, width = 12, height = 10.5, type = "cairo-png", dpi=1500)
+
 
 
 
